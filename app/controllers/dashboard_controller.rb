666ecc9851e2e251fbea7c19f3e06497fc065903
Jon Keam <jpkeam@gmail.com>
2025-08-30 16:40:47 -0400

    add filter to reports

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,45 @@
 class DashboardController < ApplicationController
   # GET /
   def index
+    ignore_categories = %i[
+      category_payment
+      category_credit
+      category_debit
+      category_installment
+      category_interest
+      category_other
+      category_income
+      category_transfer
+      category_deposit
+      category_withdrawl
+      category_dividend
+      category_rental_property
+      category_software
+    ]
+
+    @sigother = (params[:sigother] || '').strip == 'true'
+
+    @startdate = nil
+    if params[:startdate].strip != ''
+      @startdate = Date.strptime(params[:startdate], "%Y-%m-%d")
+    end
+    @endddate = nil
+    if params[:enddate].strip != ''
+      @enddate = Date.strptime(params[:enddate], "%Y-%m-%d")
+    end
+
+    unless @sigother
+      ignore_categories << :category_significant_other
+    end
+
+    @transactions = Transaction.where.not(category: ignore_categories)
+    @all_transactions = Transaction
+    if @startdate != nil
+      @transactions = @transactions.where('transaction_date >= ?', @startdate)
+      @all_transactions = @all_transactions.where('transaction_date >= ?', @startdate)
+    end
+    if @enddate != nil
+      @all_transactions = @all_transactions.where('transaction_date <= ?', @enddate)
+    end
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,24 +6,26 @@ class Transaction < ApplicationRecord
     category_interest: 3,
     category_credit: 4,
     category_debit: 5,
-    category_deposit: 6,
-    category_withdrawl: 7,
-    category_income: 8,
-    category_dividend: 9,
-    category_shopping: 10,
-    category_restaurants: 11,
-    category_services: 12,
-    category_grocery: 13,
-    category_travel: 14,
-    category_transportation: 15,
-    category_health: 16,
-    category_alcohol: 17,
-    category_entertainment: 18,
-    category_investment: 19,
-    category_rent: 20,
-    category_rental_property: 21,
-    category_significant_other: 22,
-    category_software: 23
+    category_transfer: 6,
+    category_deposit: 7,
+    category_withdrawl: 8,
+    category_income: 9,
+    category_dividend: 10,
+    category_shopping: 11,
+    category_restaurants: 12,
+    category_services: 13,
+    category_grocery: 14,
+    category_utility: 15,
+    category_travel: 16,
+    category_transportation: 17,
+    category_health: 18,
+    category_alcohol: 19,
+    category_entertainment: 20,
+    category_investment: 21,
+    category_rent: 22,
+    category_rental_property: 23,
+    category_significant_other: 24,
+    category_software: 25
   }
   enum :transaction_type, {
     type_purchase: 0,
@@ -31,7 +33,8 @@ class Transaction < ApplicationRecord
     type_installment: 2,
     type_interest: 3,
     type_credit: 4,
-    type_debit: 5
+    type_debit: 5,
+    type_transfer: 6
   }
   monetize :amount_cents
   belongs_to :bank
