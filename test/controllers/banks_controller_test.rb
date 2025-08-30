20a18f5571e7d4bfd794577e76ce14cd46a6ba19
Jon Keam <jpkeam@gmail.com>
2025-08-30 18:13:23 -0400

    fix tests

--- test/controllers/banks_controller_test.rb
+++ test/controllers/banks_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class BanksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get banks_index_url
+  test "should get listing" do
+    get banks_url
     assert_response :success
   end
 end
