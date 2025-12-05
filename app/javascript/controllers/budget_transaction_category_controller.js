123757136b0f4724691e7308f27dc32583580638
Jon Keam <jpkeam@gmail.com>
2025-12-05 18:17:42 -0500

    clean up budget form

--- app/javascript/controllers/budget_transaction_category_controller.js
+++ app/javascript/controllers/budget_transaction_category_controller.js
@@ -2,19 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="budget-transaction-category"
 export default class extends Controller {
-  static targets = ["links", "template"]
+  static targets = ["links", "template"];
 
   add_association(event) {
-    event.preventDefault()
-    console.log(event)
-    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
-    this.linksTarget.insertAdjacentHTML('beforebegin', content)
+    event.preventDefault();
+    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
+    this.linksTarget.insertAdjacentHTML('beforeend', content);
   }
 
   remove_association(event) {
-    event.preventDefault()
-    let item = event.target.closest(".budget-transaction-category")
-    item.querySelector("input[name*='_destroy']").value = 1
-    item.style.display = 'none'
+    event.preventDefault();
+    let item = event.target.closest(".budget-transaction-category");
+    item.querySelector("input[name*='_destroy']").value = 1;
+    item.style.display = 'none';
   }
 }
