c65b32c9c8a42e1e2f8308a3ce3778b83c35ee1a
Jon Keam <jpkeam@gmail.com>
2025-12-05 22:44:13 -0500

    fixed pagination and order of budget

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,5 +1,5 @@
 class Budget < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
-  has_many :budget_transaction_categories, inverse_of: :budget, dependent: :destroy
+  has_many :budget_transaction_categories, -> { order(amount_cents: :desc) }, inverse_of: :budget, dependent: :destroy
   accepts_nested_attributes_for :budget_transaction_categories, reject_if: :all_blank, allow_destroy: true
 end
